4c657a040a2656f0b718ea45b192684fd12234d6
Reverson Dias <reverino@gmail.com>
2021-01-25 19:56:01 -0300

    Change user_data to base64 encode

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.ec2_app_sg.id,data.aws_security_groups.default_vpc_sg_id.ids[0]]
   subnet_id = aws_subnet.private-subnet[0].id
 
-  user_data = file("userdata.sh")
+  user_data_base64 = filebase64("${path.module}/userdata.sh")
 
   tags = {
     Name = var.ec2_name
