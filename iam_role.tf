97ee74d8d4cbc80f9fb386e9b9e81dd508b7ca8d
Reverson Dias <reverino@gmail.com>
2021-01-25 19:17:26 -0300

    Fix and improve about IAM policies and launch instance

--- iam_role.tf
+++ iam_role.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "s3-role" {
       {
         "Action": "sts:AssumeRole",
         "Principal": {
-          "Service": "s3.amazonaws.com"
+          "Service": "ec2.amazonaws.com"
         },
         "Effect": "Allow",
         "Sid": ""
@@ -18,50 +18,14 @@ resource "aws_iam_role" "s3-role" {
   EOF
 }
 
-# resource "aws_iam_role_policy" "s3-role-policy" {
-#   name = "s3-access-role-policy"
-#   role = aws_iam_role.s3-role.id
-
-#   policy = <<-EOF
-#   {
-#     "Id": "Policy1611597205924",
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#         "Sid": "Stmt1611597201485",
-#         "Action": "s3:*",
-#         "Effect": "Allow",
-#         "Resource": "${var.arn_s3}/*",
-#         "Principal": {
-#           "AWS": [
-#             "ec2"
-#           ]
-#         }
-#       }
-#     ]
-#   }
-#   EOF
-# }
-
-resource "aws_iam_policy" "s3-role-policy" {
-  name        = "s3-access-role-policy"
-  description = "Policy to EC2 access S3"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "s3:*",
-      "Effect": "Allow",
-      "Resource": "${var.arn_s3}/*"
-    }
-  ]
-}
-EOF
+resource "aws_iam_instance_profile" "ec2-acess-profile" {
+  name = aws_iam_role.s3-role.name
+  role = aws_iam_role.s3-role.name
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.s3-role.name
-  policy_arn = aws_iam_policy.s3-role-policy.arn
+resource "aws_iam_role_policy" "s3-role-policy" {
+  name = "s3-access-role-policy"
+  role = aws_iam_role.s3-role.id
+
+  policy = templatefile("s3_policy_json_template.tpl", {aws_account_id = var.aws_account_id, s3_name = var.s3_name})
 }
